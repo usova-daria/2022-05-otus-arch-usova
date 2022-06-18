e01ce908a40acb9544c176f9f218f3db2e0f85ec
dusova <daria.usova@telekom.com>
2022-06-18 17:38:33 +0300

    Test discriminant less than epsilon

--- hw-1/src/test/java/org/otus/arch/colvers/DiscriminantQuadraticEquationSolverTest.java
+++ hw-1/src/test/java/org/otus/arch/colvers/DiscriminantQuadraticEquationSolverTest.java
@@ -39,6 +39,16 @@ class DiscriminantQuadraticEquationSolverTest {
         testSolution(-1, solution.getSolutions()[0]);
     }
 
+    @Test
+    @DisplayName("Equation with one root, discriminant is less than epsilon")
+    void solveEquationWithOneRoot_DiscriminantNotZero() {
+        QuadraticEquation equation = QuadraticEquation.from(1, Math.sqrt(DoubleUtils.EPSILON) / 10, 0);
+        EquationSolution solution = solver.solve(equation);
+
+        assertEquals( 1, solution.getSolutionsCount() );
+        testSolution(- Math.sqrt(DoubleUtils.EPSILON) / 20, solution.getSolutions()[0]);
+    }
+
     private static void testSolution(double expected, double actual) {
         assertTrue(DoubleUtils.areEqual(expected, actual), expected + " expected but found " + actual);
     }
