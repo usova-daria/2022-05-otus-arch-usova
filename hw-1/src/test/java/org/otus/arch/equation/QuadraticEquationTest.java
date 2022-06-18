b5cfdfdbfd4ed9fc9869e3bce2deb0368079ded6
dusova <daria.usova@telekom.com>
2022-06-18 20:10:25 +0300

    Add test for a less than epsilon

--- hw-1/src/test/java/org/otus/arch/equation/QuadraticEquationTest.java
+++ hw-1/src/test/java/org/otus/arch/equation/QuadraticEquationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.otus.arch.utils.DoubleUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,8 +12,14 @@ class QuadraticEquationTest {
 
     @Test
     @DisplayName("a must not be null")
-    void testFromInvalidArguments() {
+    void testFromInvalidA_zero() {
         assertThrows(IllegalArgumentException.class, () -> QuadraticEquation.from(0, 1, 1));
+    }
+
+    @Test
+    @DisplayName("a must not be less than epsilon")
+    void testFromInvalidA_epsilon() {
+        assertThrows(IllegalArgumentException.class, () -> QuadraticEquation.from(DoubleUtils.EPSILON / 2, 1, 1));
     }
 
     @ParameterizedTest
