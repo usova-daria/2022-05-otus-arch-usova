66e55181eafe31c3fd5eeff06d5d3da4a6854575
dusova <daria.usova@telekom.com>
2022-06-18 20:04:33 +0300

    Add test for special double values

--- hw-1/src/test/java/org/otus/arch/equation/QuadraticEquationTest.java
+++ hw-1/src/test/java/org/otus/arch/equation/QuadraticEquationTest.java
@@ -2,6 +2,8 @@ package org.otus.arch.equation;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +13,16 @@ class QuadraticEquationTest {
     @DisplayName("a must not be null")
     void testFromInvalidArguments() {
         assertThrows(IllegalArgumentException.class, () -> QuadraticEquation.from(0, 1, 1));
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = { Double.NaN, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY})
+    @DisplayName("Equation with not acceptable coefficients")
+    void testFromNotAcceptableCoefficients(double d) {
+        assertThrows(IllegalArgumentException.class, () -> QuadraticEquation.from(d, 1, 1));
+        assertThrows(IllegalArgumentException.class, () -> QuadraticEquation.from(1, d, 1));
+        assertThrows(IllegalArgumentException.class, () -> QuadraticEquation.from(1, 1, d));
+        assertThrows(IllegalArgumentException.class, () -> QuadraticEquation.from(d, d, d));
     }
 
 }
